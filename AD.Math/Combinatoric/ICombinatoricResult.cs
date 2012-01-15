1f82200f62d1a05944f8731d2b42176f6cadb7ef
Alberto Dallagiacoma <albyd74@hotmail.com>
2012-01-15 18:52:54 +0100

    Added AD.Math Project. removed reference to ObjectBuilder2 library in the AD.IoC.Unity project.

---
+++ AD.Math/Combinatoric/ICombinatoricResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AD.Math.Combinatoric
+{
+    /// <summary>
+    /// Interface for Permutations, Combinations and any other classes that present
+    /// a collection of collections based on an input collection.  The enumerators that 
+    /// this class inherits defines the mechanism for enumerating through the collections.  
+    /// </summary>
+    /// <typeparam name="T">The of the elements in the collection, not the type of the collection.</typeparam>
+    public interface ICombinatoricResult<T> : IEnumerable<IList<T>>
+    {
+        /// <summary>
+        /// The count of items in the collection.  This is not inherited from
+        /// ICollection since this meta-collection cannot be extended by users.
+        /// </summary>
+        long Count { get; }
+
+        /// <summary>
+        /// The type of the result, determining how the collections are 
+        /// determined from the inputs.
+        /// </summary>
+        GenerateOption Type { get; }
+
+        /// <summary>
+        /// The size of the input collection.
+        /// </summary>
+        int NElements { get; }
+
+        /// <summary>
+        /// The size of each output collection.
+        /// </summary>
+        int ClassK { get; }
+    }
+}
